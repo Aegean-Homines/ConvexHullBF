88e9168e57f9a681c8384ce5619afed85ca6b0fa
Egemen Koku <ekoku40@gmail.com>
2017-04-16 17:36:03 -0700

    Done

--- hull-bruteforce.cpp
+++ hull-bruteforce.cpp
@@ -18,8 +18,12 @@
 * @param arg2 Point to be checked with
 * @returns true if this and arg2 is equal, false otherwise
 */
-bool Point::operator==( Point const& arg2 ) const {
-    return ( (x==arg2.x) && (y==arg2.y) );
+bool Point::operator==(Point const& arg2) const {
+	return ((x == arg2.x) && (y == arg2.y));
+}
+
+bool Point::operator< (Point const& arg2) const {
+	return ((x*x + y*y) < (arg2.x * arg2.x + arg2.y * arg2.y));
 }
 
 /**
@@ -58,21 +62,21 @@ std::istream& operator>> (std::istream& os, Point & p) {
 * @param qy y coordinate of the point to be checked
 * @returns pair of isOnLeft - isOnRight
 */
-std::pair<bool,bool> get_location(
-		float const& p1x, //check which side of the line (p1x,p1y)-->(p2x,p2y) 
-		float const& p1y, //point (qx,qy) is on
-		float const& p2x,
-		float const& p2y,
-		float const& qx,
-		float const& qy
-		) 
+std::pair<bool, bool> get_location(
+	PointType const& p1x, //check which side of the line (p1x,p1y)-->(p2x,p2y) 
+	PointType const& p1y, //point (qx,qy) is on
+	PointType const& p2x,
+	PointType const& p2y,
+	PointType const& qx,
+	PointType const& qy
+	)
 {
-	Point dir   = {p2x-p1x,p2y-p1y};
-	Point norm  = {dir.y, -dir.x};
-	Point point = {qx-p1x,qy-p1y};
+	Point dir = { p2x - p1x,p2y - p1y };
+	Point norm = { dir.y, -dir.x };
+	Point point = { qx - p1x,qy - p1y };
 	//scalar product is positive if on right side
-	float scal_prod = norm.x*point.x + norm.y*point.y;
-	return std::make_pair( (scal_prod<0), (scal_prod>0));
+	PointType scal_prod = norm.x*point.x + norm.y*point.y;
+	return std::make_pair((scal_prod<0), (scal_prod>0));
 }
 
 // Well apparently the function above also does this but in a cooler way
@@ -89,15 +93,15 @@ bool checkPoints(unsigned int point1Index, unsigned int point2Index, std::vector
 	bool isOnPositiveSide = false;
 	Point const & point1 = points[point1Index];
 	Point const & point2 = points[point2Index];
-	float xDiff = point2.x - point1.x;
-	float yDiff = point2.y - point1.y;
+	PointType xDiff = point2.x - point1.x;
+	PointType yDiff = point2.y - point1.y;
 
 	for (unsigned int i = 0; i < points.size(); ++i) {
 		if (i == point1Index || i == point2Index)
 			continue;
 
 		Point const & pointToCheck = points[i];
-		float lineEquationResult = (pointToCheck.y - point1.y) * xDiff - (pointToCheck.x - point1.x) * yDiff;
+		PointType lineEquationResult = (pointToCheck.y - point1.y) * xDiff - (pointToCheck.x - point1.x) * yDiff;
 		if (lineEquationResult > 0)
 			isOnPositiveSide = true;
 		else if (lineEquationResult < 0)
@@ -105,7 +109,7 @@ bool checkPoints(unsigned int point1Index, unsigned int point2Index, std::vector
 
 		if (isOnNegativeSide && isOnPositiveSide)
 			return false;
-			
+
 	}
 
 	return true;
@@ -116,20 +120,20 @@ bool checkPoints(unsigned int point1Index, unsigned int point2Index, std::vector
 * @param points all points on the plane
 * @returns a set of indices of points that form convex hull
 */
-std::set<int> hullBruteForce ( std::vector< Point > const& points ) {
+std::set<int> hullBruteForce(std::vector< Point > const& points) {
 	int num_points = points.size();
-	if ( num_points < 3 ) throw "bad number of points";
+	if (num_points < 3) throw "bad number of points";
 
 	std::set<int> hull_indices;
 	for (unsigned int i = 0; i < points.size(); ++i) { // For each point in the points
 		for (unsigned int j = i + 1; j < points.size(); ++j) { // Try all the other points in the points
-			if(checkPoints(i, j, points)) {
+			if (checkPoints(i, j, points)) {
 				hull_indices.insert(i);
 				hull_indices.insert(j);
 			}
 		}
 	}
-		
+
 	return hull_indices;
 }
 
@@ -142,14 +146,14 @@ void findHullVertexRec(std::vector<int> & hullIndices, std::vector< Point > cons
 	int lastVertex = hullIndices.back();
 
 	bool sameSide = true;
-	float lastVertexX = points[lastVertex].x;
-	float lastVertexY = points[lastVertex].y;
-	float possibleVertexX, possibleVertexY;
+	int lastVertexX = points[lastVertex].x;
+	int lastVertexY = points[lastVertex].y;
+	int possibleVertexX, possibleVertexY;
 
 	// have to do this to get rid of all the sign-compare warnings
 	int pointsSize = points.size();
 
-	for (int i = pointsSize-1; i >= 0; --i) {
+	for (int i = pointsSize - 1; i >= 0; --i) {
 		if (i == lastVertex)
 			continue;
 		possibleVertexX = points[i].x;
@@ -157,11 +161,11 @@ void findHullVertexRec(std::vector<int> & hullIndices, std::vector< Point > cons
 		for (int k = 0; k < pointsSize && sameSide; ++k) {
 			if (k == i || k == lastVertex)
 				continue;
-
-			sameSide = sameSide && (!get_location(lastVertexX, lastVertexY, possibleVertexX, possibleVertexY, points[k].x, points[k].y).second);
+			std::pair<bool, bool> locationResult = get_location(lastVertexX, lastVertexY, possibleVertexX, possibleVertexY, points[k].x, points[k].y);
+			sameSide = sameSide && (!locationResult.second);
 		}
 
-		if(sameSide) {
+		if (sameSide) {
 			hullIndices.push_back(i);
 			break;
 		}
@@ -183,18 +187,18 @@ void findHullVertexRec(std::vector<int> & hullIndices, std::vector< Point > cons
 * @param points all points on the plane
 * @returns a vector of indices of points that form convex hull
 */
-std::vector<int> hullBruteForce2 ( std::vector< Point > const& points ) {
+std::vector<int> hullBruteForce2(std::vector< Point > const& points) {
 	int num_points = points.size();
-	if ( num_points < 3 ) throw "bad number of points";
+	if (num_points < 3) throw "bad number of points";
 
 	int smallestPointIndex;
 	int xMin = std::numeric_limits<int>::max();
 	std::vector<int> hull_indices;
-	float currentX;
+	int currentX;
 	for (unsigned int i = 0; i < points.size(); ++i) {
 		currentX = points[i].x;
 
-		if(currentX < xMin ) {
+		if (currentX < xMin) {
 			smallestPointIndex = i;
 			xMin = currentX;
 		}
